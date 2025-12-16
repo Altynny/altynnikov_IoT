4dfffba67370a5f6c004698684361177adfd9235
Ilya <altynnikovila@gmail.com>
2025-12-16 08:07:32 +0800

    Rename 3 blink types and 8 bit blink because of mix up

--- blink_timers/3_blink_types.ino
+++ blink_timers/3_blink_types.ino
@@ -1,4 +1,9 @@
 volatile bool state = false;
+const char TIMER_MODE = 'T';
+const char MILLIS_MODE = 'M';
+const char DELAY_MODE = 'D';
+char mode = TIMER_MODE;
+unsigned long start = 0;
 
 void setup() {
   cli();
@@ -7,13 +12,52 @@ void setup() {
   TCCR2B = TCCR2B | ((1 << CS22) | (1 << CS21) | (1 << CS20));
   TIMSK2 = TIMSK2 | (1 << TOIE2);
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
   sei();
 }
 
 ISR(TIMER2_OVF_vect) {
-  state = !state; 
+  if (mode == TIMER_MODE)
+  	state = !state; 
+}
+
+void updateMode() {
+   if (Serial.available() > 0) {
+    	char new_mode = Serial.read();
+     	switch (new_mode) {
+     		case TIMER_MODE:
+          		mode = new_mode;
+          		Serial.println("LED goes TIMER_MODE");
+          		break;
+          	case MILLIS_MODE:
+		        mode = new_mode;
+          		Serial.println("LED goes MILLIS_MODE");
+          		start = millis();
+          		break;
+          	case DELAY_MODE:
+          		mode = new_mode;
+          		Serial.println("LED goes DELAY_MODE");
+          		break;
+        } 
+  }
+}
+
+void handleLED() {
+  if (mode == MILLIS_MODE) {
+    unsigned long end = millis();
+    if (end - start >= 250) {
+      state = !state;
+      start = end;
+    }
+  } 
+  else if (mode == DELAY_MODE) {
+     delay(50);
+     state = !state;
+  }
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, state); 
+  	updateMode();
+  	handleLED();
+  	digitalWrite(LED_BUILTIN, state);
 }
